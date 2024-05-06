61d182df065e3709f7526de251df6a9b1c758cb9
NadegeL <nadege.luthier@gmail.com>
2024-05-06 12:41:46 -0700

    corriger while

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 import sys
 
@@ -6,9 +5,8 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n = - 1
+        n -= 1
     return result
 
 f = factorial(int(sys.argv[1]))
 print(f)
-
